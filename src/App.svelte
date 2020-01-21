d447e566d462a5dd0f73b33bae3b3c2e30cf3237
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-01-20 22:59:23 -0300

    Added apollo client for application.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
+  import ApolloClient from "apollo-boost";
+  import { setClient } from "svelte-apollo";
   import Header from "./Header.svelte";
   import LinkList from "./LinkList.svelte";
+
+  const client = new ApolloClient({ uri: "http://localhost:4000" });
+  setClient(client);
 </script>
 
 <style>
