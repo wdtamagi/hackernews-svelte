2a71dd1c8d86d5c1841365cf1d1cb687465c9028
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-01-27 22:40:12 -0300

    Fixed app bar.

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,8 @@
   import { Router, Route } from "svelte-routing";
   import ApolloClient from "apollo-boost";
   import { setClient } from "svelte-apollo";
-  import Header from "./Header.svelte";
-  import LinkList from "./LinkList.svelte";
+  import Header from "./components/Header.svelte";
+  import LinkList from "./components/LinkList.svelte";
 
   const client = new ApolloClient({ uri: "http://localhost:4000" });
   setClient(client);
@@ -17,12 +17,14 @@
   }
 </style>
 
-<Router {url}>
-  <Header />
-  <Route path="/" component={LinkList} />
-  <Route path="new/:page" component={LinkList} />
-  <Route path="top" component={LinkList} />
-  <Route path="search" component={LinkList} />
-  <Route path="login" component={LinkList} />
-  <Route path="create" component={LinkList} />
-</Router>
+<div>
+  <Router {url}>
+    <Header />
+    <Route path="/" component={LinkList} />
+    <Route path="new/:page" component={LinkList} />
+    <Route path="top" component={LinkList} />
+    <Route path="search" component={LinkList} />
+    <Route path="login" component={LinkList} />
+    <Route path="create" component={LinkList} />
+  </Router>
+</div>
