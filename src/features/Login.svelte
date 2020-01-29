ea289779dd615f3a031e4a40b8c020c75ee66294
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-01-28 23:11:37 -0300

    Added login and create account forms.

--- src/features/Login.svelte
+++ src/features/Login.svelte
@@ -1,11 +1,41 @@
 <script>
-  // your script goes here
+  export let router = {};
 </script>
 
 <style>
-  /* your styles go here */
+  .labels {
+    width: 150px;
+  }
+
+  .inputs {
+    width: 300px;
+  }
+
+  .button {
+    width: 150px;
+  }
 </style>
 
 <div>
-  <span />
+  <h2>Login</h2>
+  <div>
+    <div class="labels">Username:</div>
+    <input class="inputs" type="text" />
+  </div>
+  <div>
+    <div class="labels">Password:</div>
+    <input class="inputs" type="password" />
+  </div>
+  <button class="button">Login</button>
+
+  <h2>Create account</h2>
+  <div>
+    <div class="labels">Username:</div>
+    <input class="inputs" type="text" />
+  </div>
+  <div>
+    <div class="labels">Password:</div>
+    <input class="inputs" type="password" />
+  </div>
+  <button class="button">Create account</button>
 </div>
