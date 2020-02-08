5c8ca009c6b04af38994d10c681d673b8875dea6
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-02-08 18:44:38 -0300

    moved components as features.

---
+++ src/features/login/Login.svelte
@@ -0,0 +1,20 @@
+<script>
+  import SignIn from "../../components/SignIn.svelte";
+  import SignUp from "../../components/SignUp.svelte";
+
+  let loginContext = "login";
+
+  const changeContext = event => (loginContext = event.detail.text);
+</script>
+
+<style>
+
+</style>
+
+<div>
+  {#if loginContext === 'login'}
+    <SignIn on:changeContext={changeContext} />
+  {:else}
+    <SignUp on:changeContext={changeContext} />
+  {/if}
+</div>
